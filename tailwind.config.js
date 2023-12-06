3a19dd3d1402baf443f53b205f4019b2528a179d
rizkyberutu <rizkyy0103@gmail.com>
2023-12-07 03:30:11 +0700

    commit pertama

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html", "blog.html", "pesan.html", "galeri.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary: '#64748b',
-        dark: '#0f172a'
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#070C29",
       },
-      screens:{
-        '2xl': '1320px',
+      fontFamily: {
+        body: ["Poppins"],
+        head: ["Yeseva One"],
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
-
+};
